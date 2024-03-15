1fbf060de6cce8716cd2b3392fa0d490d6c39a4e
AlvaroCastaneda145 <alvaro18wakanda@gmail.com>
2024-03-15 15:49:18 -0500

    api.py

--- api.py
+++ api.py
@@ -16,14 +16,36 @@ configRemote={
 }
 
 cnx = mysql.connector.connect(**config)
-
+cursor = cnx.cursor()
 app = Flask(__name__)
 
 @app.route("/cines")
-def index():
-    return"Cines"
+def cines():
+    cursor.execute('call sp_getCines()')
+    cines = cursor.fetchall()
+    cursor.close()
+    return cines
+
+@app.route("/peliculas/<id>")
+def peliculas_cartelera(id):
+    return "Hola id"
+
+@app.route("/peliculas/<int:id>")
+def peliculas_cartelera(id):
+    return "int id"
+
+# @app.route("/peliculas/cartelera")
+# def peliculas_cartelera():
+#     return"Cartelera"
+
+@app.route("/peliculas/estrenos")
+def peliculas_estrenos():
+    return"Estrenos"
+
 
 if __name__ == '__main__' :
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
+
+    
 
     
